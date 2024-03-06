587da844e3e922afbf215cf15e4bbdde1ecd695b
Ryszard Błażej <sapermysza@gmail.com>
2024-03-06 12:36:25 +0100

    Added sample loading from .obj by path

---
+++ Assets/Scripts/ModelLoading/ModelLoader.cs
@@ -0,0 +1,30 @@
+using Dummiesman;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DeployableObject
+{
+    public Vector3 location;
+    public string path;
+}
+
+public class ModelLoader : MonoBehaviour
+{
+    public List<DeployableObject> objectsToDeploy;
+
+    private void Start()
+    {
+        InitialzeModels(objectsToDeploy);
+    }
+
+    private void InitialzeModels(List<DeployableObject> objectsToDeploy)
+    {
+        foreach (var deployableObject in objectsToDeploy)
+        {
+            var loadedObj = new OBJLoader().Load(deployableObject.path);
+            loadedObj.transform.position = deployableObject.location;
+        }
+    }
+}
