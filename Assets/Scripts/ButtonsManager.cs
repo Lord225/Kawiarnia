5ea765bf4fc5c03b4af36e96b2404f97143421ec
zarebajakub <jakubzareba2001@gmail.com>
2024-04-30 07:26:55 +0200

    UI loading scene

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonsManager : MonoBehaviour
+{
+    //public InputField InputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        Debug.Log("Load btn pressed");// + InputField.text);
+
+        SceneManager.LoadScene("baseScene");
+    }
+
+}
